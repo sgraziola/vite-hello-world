c460e9667286493d337206acf3a1bf8b6a6a38d2
sgraziola <simo.fdl@gmail.com>
2022-11-09 22:07:43 +0100

    Vite Ex: Add script exports to components

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,3 +1,9 @@
+<script>
+export default {
+    name: 'AppHero'
+}
+</script>
+
 <template>
     <div class="jumbotron">
         <img src="../assets/og-image.png" alt="">
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,3 +1,9 @@
+<script>
+export default {
+  name: 'AppTitle'
+}
+</script>
+
 <template>
   <h1 class="gradient_text">La mia prima app con Vite</h1>
 </template>
