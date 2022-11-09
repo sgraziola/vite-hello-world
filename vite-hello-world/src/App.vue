8e225216e54ecb09ca5da4c22a28325528e56779
sgraziola <simo.fdl@gmail.com>
2022-11-09 14:14:26 +0100

    Vite Ex: add title in dom

--- vite-hello-world/src/App.vue
+++ vite-hello-world/src/App.vue
@@ -1,12 +1,14 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
   export default{
-    components: AppTitle
+    components: {
+      AppTitle
+    }
   }
 </script>
 
 <template>
-  <AppTitle />
+  <AppTitle></AppTitle>
 </template>
 
 <style></style>
--- vite-hello-world/src/components/AppTitle.vue
+++ vite-hello-world/src/components/AppTitle.vue
@@ -1,3 +1,3 @@
 <template>
     <h1>La mia prima app con Vite</h1>
-  </template>
+</template>
