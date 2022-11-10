4e8d7e7d6d4d6d20ac3234cd09451f16bac5ea17
sgraziola <simo.fdl@gmail.com>
2022-11-10 10:05:31 +0100

    Vite Ex: bug fix string App

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import AppTitle from './components/AppTitle.vue';
 import AppHero from './components/AppHero.vue'
 export default {
-  name: App,
+  name: 'App',
   components: {
     AppTitle,
     AppHero,
